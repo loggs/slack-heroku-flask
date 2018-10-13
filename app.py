d5e744ad6f9322c4c6d448ff96e1c473dde7003e
suguloga <lcsugit@gmail.com>
2018-10-12 20:52:49 -0700

    Add simple server

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Your ip is: <b>{}</b>'.format(request.remote_addr)
 
 if __name__ == '__main__':
     app.run(port=3000)
