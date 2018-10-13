b5494b91acc7c6c1e249398dc844d12b69f7c256
suguloga <lcsugit@gmail.com>
2018-10-12 21:01:19 -0700

    Small tweaks

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Your ip is: <b>{}</b>'.format(request.remote_addr)
+    return 'Your IP is: <b>{}</b>'.format(request.remote_addr)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
