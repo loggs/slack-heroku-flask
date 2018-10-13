24d2815028b07b78827b7c089ee5f206fe88e341
suguloga <lcsugit@gmail.com>
2018-10-12 20:56:53 -0700

    Add heroku dependencies

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, request
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,4 +8,5 @@ def hello_world():
     return 'Your ip is: <b>{}</b>'.format(request.remote_addr)
 
 if __name__ == '__main__':
-    app.run(port=3000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
